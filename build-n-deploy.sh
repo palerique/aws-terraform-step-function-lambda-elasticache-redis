b0e6c3d52abcc14beb1c1c9de03606d02b1e3bee
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2021-01-23 23:09:37 -0300

    refactoring it

--- build-n-deploy.sh
+++ build-n-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pushd lambda/influence-analysis-II || exit 1
+pushd lambda/influence-analysis || exit 1
 set -e
 ./gradlew clean build
 popd || exit 1
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,8 +3,8 @@ locals {
   lambda_memory = 256
   runtime       = "java11"
 
-  layer_jar_path  = "${path.module}/../lambda/influence-analysis-II/layer/build/libs/layer.jar"
-  lambda_jar_path = "${path.module}/../lambda/influence-analysis-II/lambda/build/libs/lambda.jar"
+  layer_jar_path  = "${path.module}/../lambda/influence-analysis/layer/build/libs/layer.jar"
+  lambda_jar_path = "${path.module}/../lambda/influence-analysis/lambda/build/libs/lambda.jar"
 
   tags = {
     Name    = "influence-analysis"
